3fe34dc9a76d3a2cf8e1a3d705e0714eca02b705
ZILECAO <zilecao@sas.upenn.edu>
2025-02-01 19:16:31 -0800

    remove unnecessary types

--- botbbles/src/dunePlugin/types/metadata.ts
+++ botbbles/src/dunePlugin/types/metadata.ts
@@ -3,31 +3,24 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export interface DuneMetadata extends RecordMetadata {
+  chartTitle: string;
+  chartDescription: string;
   queryId: string;
   timestamp: string;
   type: 'dune_metrics';
   [key: string]: string | number | boolean;
 }
 
-
-export type ChartMetadata = DuneMetadata;
-
 export interface DunePineconeRecord extends ScoredPineconeRecord<DuneMetadata> {
   metadata: DuneMetadata;
 }
 
-export interface ChartQueryResult extends ScoredPineconeRecord<ChartMetadata> {
+export interface ChartQueryResult extends ScoredPineconeRecord<DuneMetadata> {
   id: string;
   values: number[];
   score: number;
-  metadata: ChartMetadata;
+  metadata: DuneMetadata;
 }
-
-export interface ChartFilter {
-  type: { $eq: 'dune_metrics' };
-  chartTitle?: { $eq: string };
-}
-
 
 export interface ChartContext {
   title: string;
