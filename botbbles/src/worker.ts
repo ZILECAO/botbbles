1a32c40e58dd5cf1a5a1adadb70e2204d1969d65
Zile <75930156+ZILECAO@users.noreply.github.com>
2025-02-02 10:19:40 -0800

    read Worker impl (#11)
    * add impl for fine tune worker
    
    * impl readTweetWorker

--- botbbles/src/worker.ts
+++ botbbles/src/worker.ts
@@ -1,5 +1,5 @@
 import { GameWorker } from "@virtuals-protocol/game";
-import { searchBotbblesTweetsAndExtractDuneQueryIdFunction, replyToTweetFunction, postTweetFunction, upsertDuneToPineconeFunction } from "./functions";
+import { searchBotbblesTweetsAndExtractDuneQueryIdFunction, replyToTweetFunction, postTweetFunction, upsertDuneToPineconeFunction, fineTuneFunction } from "./functions";
 import { twitterPlugin } from "./plugins/twitterPlugin/twitterPlugin";
 import { FineTuningManager } from "./scripts/unsloth-finetune/fineTune";
 
@@ -17,17 +17,19 @@ export const postTweetWorker = new GameWorker({
     },
 });
 
-// TODO: Implement read tweet worker
 export const readTweetWorker = new GameWorker({
     id: "read_tweet_worker",
     name: "Read Tweet worker",
-    description: "A worker that reads tweets and looks for mentions of @Botbbles with a Dune Analytics chart URL. Returns the Query ID of the Dune Analytics chart.",
+    description: "A worker that reads tweets and looks for mentions of @Botbbles with a URL. Check if the URL is a Dune Analytics chart URL. If so, returns the Query ID of the Dune Analytics chart.",
     functions: [
         searchBotbblesTweetsAndExtractDuneQueryIdFunction
     ],
     getEnvironment: async () => {
+        // Get the last task's response from the worker's state
+        const lastTaskResponse = await searchBotbblesTweetsAndExtractDuneQueryIdFunction;
+        
         return {
-            dune_query_id: 0, //TODO
+            dune_query_id: lastTaskResponse || 0,
         };
     },
 });
@@ -46,12 +48,13 @@ export const duneRAGWorker = new GameWorker({
     },
 });
 
-// TODO: Implement fine-tuning worker
+// TODO: Didn't have time to implement this for the hackathon demo, but can easily be incorporated into the workflow. 
+// See fineTuneFunction in functions.ts for the implementation.
 export const fineTuneWorker = new GameWorker({
     id: "fine_tune_worker",
     name: "Fine Tuning Worker",
     description: "Monitors performance and triggers fine-tuning for self-improvement. The fine-tuning flow goes as follows: 1) worker runs the function to convert pinecone data to training examples 2) worker runs the function to remote fine-tune the model 3) worker returns status success or failure",
-    functions: [], // TODO: Implement fine-tuning function
+    functions: [fineTuneFunction], 
     getEnvironment: async () => {
         const manager = new FineTuningManager();
         const storedEnv = {
